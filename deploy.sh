412f39096b7219d9975bd3d3efb4dc6f402f7854
Bertrand Fan <bertrand@fan.net>
2023-12-27 16:10:15 -0800

    update log dir, exclude deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -av --exclude .venv --exclude .git --exclude .mypy_cache --exclude poetry.lock ./ web@nuke:/web/key-nexus/
+rsync -av --exclude-from=.gitignore --exclude .git ./ web@nuke:/web/key-nexus/
 ssh web@nuke -f "ps aux | grep gunicorn | grep "main:app" | awk '{ print \$2 }' | xargs kill -HUP"
